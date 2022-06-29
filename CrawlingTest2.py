15d0421d5ab8378993d7f71da60dd95bdb4645b0
kangsm0903 <kangsm0903@naver.com>
2022-06-30 01:08:06 +0900

    Add Rating

--- CrawlingTest2.py
+++ CrawlingTest2.py
@@ -1,3 +1,5 @@
+# Rating, Content, Count 순으로 정렬
+
 import csv
 from konlpy.tag import Okt
 
@@ -8,20 +10,26 @@ f2= open('C:/Users/ksm36/Desktop/dataset/test2.csv', 'w', newline='', encoding='
 
 rdr=csv.reader(f)
 writer=csv.writer(f2)
+
+writer.writerow(['Rating','Content','Count'])
+
 result={}
+rating={}
 
 for line in rdr:
 
     target=okt.morphs(line[8]) # separate
 
     for i in target:
-        if len(i)>1:    
+        if len(i)>1:
             if i in result: # 추가
                 result[i]+=1
             else:           # 갱신
                 result[i]=1
+                rating[i]=line[6]
 
+result.pop('content')
 for i in result.items():
-    writer.writerow([i[0],i[1]])
+    writer.writerow([rating[i[0]],i[0],i[1]])
 
 f.close()
