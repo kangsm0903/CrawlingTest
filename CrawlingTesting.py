18469b06089ef65df1ae46f7b4f4be1b817e864f
kangsm0903 <kangsm0903@naver.com>
2022-06-30 03:24:20 +0900

    Crawling Test v2

--- CrawlingTesting.py
+++ CrawlingTesting.py
@@ -1,17 +1,23 @@
+# 이것저것 해봤는데 안됨, csv상에서 정리하는게 빠르다고 판단됨 - Classifying review-based data
 import csv
 from konlpy.tag import Okt
 
 okt = Okt()
 
 f = open('C:/Users/ksm36/Desktop/dataset/review_dataset.csv','r', encoding='utf-8')
-
 rdr=csv.reader(f)
+case=[]
+for i in rdr:
+    case.append(i)
 
-def solution(k,rdr):
+f2= open('C:/Users/ksm36/Desktop/dataset/test.csv', 'w', newline='', encoding='utf-8-sig')
+fieldnames=['one_score','one_value','two_score','two_value','three_score','three_value','four_score','four_value','five_score','five_value']
+writer = csv.DictWriter(f2, fieldnames=fieldnames)
+writer.writeheader()
+
+def solution(k):
     result={}
-
-    for line in rdr:
-
+    for line in case:
         if line[6]==str(k):
             target=okt.morphs(line[8]) # separate
 
@@ -25,23 +31,22 @@ def solution(k,rdr):
                 for i in result.items(): # ('A','b')
                     writer.writerow({'one_score': i[0],'one_value': i[1]})
             elif str(k)==str(2):
-                for i in result.items(): # ('A','b')
+                for i in result.items(): # 
                     writer.writerow({'two_score': i[0],'two_value': i[1]})
             elif str(k)==str(3):
-                for i in result.items(): # ('A','b')
+                for i in result.items(): # 
                     writer.writerow({'three_score': i[0],'three_value': i[1]})
             elif str(k)==str(4):
-                for i in result.items(): # ('A','b')
+                for i in result.items(): # 
                     writer.writerow({'four_score': i[0],'four_value': i[1]})
             elif str(k)==str(5):
-                for i in result.items(): # ('A','b')
+                for i in result.items(): # 
                     writer.writerow({'five_score': i[0],'five_value': i[1]})
 
-with open('C:/Users/ksm36/Desktop/dataset/test.csv', 'w', newline='', encoding='utf-8-sig') as f2:
-    fieldnames=['one_score','one_value','two_score','two_value','three_score','three_value','four_score','four_value','five_score','five_value']
-
-# writer=csv.writer(f2)
-    writer = csv.DictWriter(f2, fieldnames=fieldnames)
-    writer.writeheader()
+#solution(1)
+solution(2)
+solution(3)
+# solution(4)
+# solution(5)
 
 f.close()
